4b8f97f1886ff2972cd7314fbb4af47d4c0aa2d6
IsabellaMuroni <isa_sk_@hotmail.com>
2022-09-09 19:35:38 -0300

    Ultimos cambios

--- tp2/pto3/Program.cs
+++ tp2/pto3/Program.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Alumno.cs;
+
 
 namespace pto3 {
 
     static void Main (string [] args) {
 
-        List<Alumno> Alumnos = new List<Alumno>();
 
+        Console.WriteLine("Instituto de Educación Física");
         Console.WriteLine("Nombre: ");
+
     }
 }
