f1c47cc862df11acb093c49b0a05d759ec4e1d18
IsabellaMuroni <isa_sk_@hotmail.com>
2022-10-13 22:01:07 -0300

    Se completó la clase HelperDeArchivos

--- tp2/pto3/HelperDeArchivos.cs
+++ tp2/pto3/HelperDeArchivos.cs
@@ -10,37 +10,54 @@ namespace pto3
 {
     public static class HelperDeArchivos
     {
-        static void cargarArchivo (List<Alumno> listaAlumnos, string CsvFileName)
+        public static void cargarArchivo (List<Alumno> listaAlumnos, string csvFileName)
         {
-            string directorio = "Lista";
-
-            if(!Directory.Exists(directorio))
+            string directorio = "Lista de Alumnos";
+            if (!Directory.Exists (directorio))//Si no exixte el directorio, lo crea
             {
                 Directory.CreateDirectory(directorio);
             }
 
-            string csvFile = directorio + @"\" + CsvFileName;
+            string ruta = directorio + @"\" + csvFileName;
             string line;
 
             try
             {
                 using (StreamWriter file = File.CreateText(csvFile))
                 {
-                    foreach (Alumno alumno in listaAlumnos)
+                    foreach (Alumno alu in listaAlumnos)
                     {
-                        line = alumno.concatenarDatos();
-                        file.WriteLine(line);
+                        line = alu.concatenarDatos(); //Se guardan datos de un alumno
+                        file.WriteLine(line);//Se escribe en el archivo
                     }
-                    file.Close();
+
+                    file.Close();//Cierro archivo
                 }
+
+                Console.WriteLine ("listaalumnos" + csvFileName + "se guardó exitosamente"); 
             }
             catch (System.Exception)
             {
-                
-                Console.WriteLine("Ocurrió un error");
+                Console.WriteLine("\n\n¡Hubo un error!");
+                Console.WriteLine(error.Message);
             }
-
         }
         
+
+        //Función para cargar archivo csv
+        public static void cargarListado (List<Alumno> listaAlumnos, string fileName)
+        {
+            string [] content = File.ReadAllLines(fileName);
+            foreach (strign line in content)
+            {
+                string [] delimitedContent = line.Split(';');
+                int dni = Convert.ToInt32(delimitedContent[0]);
+                string nombre = delimitedContent[1];
+                string apellido = delimitedContent[2];
+                
+                Alumno newObject = new Alumno(dni, nombre, apellido);
+                listaAlumnos.Add(newObject);
+            }
+        }
     }
 }
